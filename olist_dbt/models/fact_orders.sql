164ecd918afb6c9c8b5ef39ff30639177c003660
My name <me@example.com>
2026-03-14 09:29:18 +0800

    Create fact and dim tables and schema.yml

---
+++ olist_dbt/models/fact_orders.sql
@@ -0,0 +1,22 @@
+{{
+	config(
+		materialized='table',
+		alias='fact_order'
+	)
+}}
+
+with src_orders as (
+	select *
+	from {{ source('olist_dataset', 'orders') }}
+)
+
+select
+	cast(order_id as string) as order_id,
+	cast(customer_id as string) as customer_id,
+	cast(order_status as string) as order_status,
+	cast(order_purchase_timestamp as timestamp) as order_purchase_timestamp,
+	cast(order_approved_at as timestamp) as order_approved_at,
+	cast(order_delivered_carrier_date as timestamp) as order_delivered_carrier_date,
+	cast(order_delivered_customer_date as timestamp) as order_delivered_customer_date,
+	cast(order_estimated_delivery_date as timestamp) as order_estimated_delivery_date
+from src_orders
