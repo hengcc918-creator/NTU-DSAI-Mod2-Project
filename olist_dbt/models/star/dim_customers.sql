baf772b1d839d8ee11f6080b97c2428e46867c40
My name <me@example.com>
2026-03-16 14:31:34 +0800

    Updated seller_zip_code_prefix to seller_zipcode

--- olist_dbt/models/star/dim_customers.sql
+++ olist_dbt/models/star/dim_customers.sql
@@ -12,7 +12,7 @@ with src_customers as (
 select distinct
 	cast(customer_id as string) as customer_id,
 	cast(customer_unique_id as string) as customer_unique_id,
-	cast(customer_zip_code_prefix as string) as customer_zip_code_prefix,
+	cast(customer_zip_code_prefix as string) as customer_zipcode,
 	cast(customer_city as string) as customer_city,
 	cast(customer_state as string) as customer_state
 from src_customers
